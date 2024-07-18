3afdc6b191d64fd8e2d0300d426e61f088b861cf
prudent13 <prudent13@gmial.com>
2024-07-18 14:08:12 +0900

    불필요한 파일 삭제

--- train.py
+++ train.py
@@ -16,8 +16,10 @@ from utils import CTCLabelConverter, CTCLabelConverterForBaiduWarpctc, AttnLabel
 from dataset import hierarchical_dataset, AlignCollate, Batch_Balanced_Dataset
 from model import Model
 from test import validation
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'디바이스: {device}')
 
 def train(opt):
     """ dataset preparation """
@@ -231,51 +233,44 @@ def train(opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_name', help='Where to store logs and models')
-    parser.add_argument('--train_data', required=True, help='path to training dataset')
-    parser.add_argument('--valid_data', required=True, help='path to validation dataset')
-    parser.add_argument('--manualSeed', type=int, default=1111, help='for random seed setting')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
-    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
-    parser.add_argument('--num_iter', type=int, default=300000, help='number of iterations to train for')
-    parser.add_argument('--valInterval', type=int, default=2000, help='Interval between each validation')
-    parser.add_argument('--saved_model', default='', help="path to model to continue training")
-    parser.add_argument('--FT', action='store_true', help='whether to do fine-tuning')
-    parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is Adadelta)')
-    parser.add_argument('--lr', type=float, default=1, help='learning rate, default=1.0 for Adadelta')
-    parser.add_argument('--beta1', type=float, default=0.9, help='beta1 for adam. default=0.9')
-    parser.add_argument('--rho', type=float, default=0.95, help='decay rate rho for Adadelta. default=0.95')
-    parser.add_argument('--eps', type=float, default=1e-8, help='eps for Adadelta. default=1e-8')
-    parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping value. default=5')
-    parser.add_argument('--baiduCTC', action='store_true', help='for data_filtering_off mode')
-    """ Data processing """
-    parser.add_argument('--select_data', type=str, default='MJ-ST',
-                        help='select training data (default is MJ-ST, which means MJ and ST used as training data)')
-    parser.add_argument('--batch_ratio', type=str, default='0.5-0.5',
-                        help='assign ratio for each selected data in the batch')
-    parser.add_argument('--total_data_usage_ratio', type=str, default='1.0',
-                        help='total data usage ratio, this ratio is multiplied to total number of data.')
-    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
-    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
-    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
-    parser.add_argument('--rgb', action='store_true', help='use rgb input')
-    parser.add_argument('--character', type=str,
-                        default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
-    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
-    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
-    parser.add_argument('--data_filtering_off', action='store_true', help='for data_filtering_off mode')
-    """ Model Architecture """
-    parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
-    parser.add_argument('--FeatureExtraction', type=str, required=True,
-                        help='FeatureExtraction stage. VGG|RCNN|ResNet')
-    parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
-    parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
-    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
-    parser.add_argument('--input_channel', type=int, default=1,
-                        help='the number of input channel of Feature extractor')
-    parser.add_argument('--output_channel', type=int, default=512,
-                        help='the number of output channel of Feature extractor')
-    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
+    parser.add_argument('--exp_name', help='로그와 모델을 저장할 위치')
+    parser.add_argument('--train_data', required=True, help='훈련 데이터셋 경로')
+    parser.add_argument('--valid_data', required=True, help='검증 데이터셋 경로')
+    parser.add_argument('--manualSeed', type=int, default=1111, help='랜덤 시드 설정')
+    parser.add_argument('--workers', type=int, help='데이터 로딩 워커 수', default=4)
+    parser.add_argument('--batch_size', type=int, default=192, help='입력 배치 크기')
+    parser.add_argument('--num_iter', type=int, default=500000, help='훈련 반복 횟수')
+    parser.add_argument('--valInterval', type=int, default=600, help='검증 간격')
+    parser.add_argument('--saved_model', default='', help="계속 훈련할 모델 경로")
+    parser.add_argument('--FT', action='store_true', help='미세 조정(fine-tuning) 수행 여부')
+    parser.add_argument('--adam', action='store_true', help='Adam 사용 여부 (기본값은 Adadelta)')
+    parser.add_argument('--lr', type=float, default=1, help='학습률, 기본값은 1.0 (Adadelta)')
+    parser.add_argument('--beta1', type=float, default=0.9, help='Adam의 beta1 값, 기본값은 0.9')
+    parser.add_argument('--rho', type=float, default=0.95, help='Adadelta의 감소율 rho, 기본값은 0.95')
+    parser.add_argument('--eps', type=float, default=1e-8, help='Adadelta의 eps 값, 기본값은 1e-8')
+    parser.add_argument('--grad_clip', type=float, default=5, help='그래디언트 클리핑 값, 기본값은 5')
+    parser.add_argument('--baiduCTC', action='store_true', help='데이터 필터링 끄기 모드')
+    """ 데이터 처리 """
+    parser.add_argument('--select_data', type=str, default='MJ-ST',help='훈련 데이터 선택 (기본값은 MJ-ST, MJ와 ST를 훈련 데이터로 사용)')
+    parser.add_argument('--batch_ratio', type=str, default='0.5-0.5',help='배치에서 각 선택된 데이터의 비율 할당')
+    parser.add_argument('--total_data_usage_ratio', type=str, default='1.0',help='전체 데이터 사용 비율, 이 비율은 전체 데이터 수에 곱해짐')
+    parser.add_argument('--batch_max_length', type=int, default=25, help='최대 라벨 길이')
+    parser.add_argument('--imgH', type=int, default=32, help='입력 이미지의 높이')
+    parser.add_argument('--imgW', type=int, default=100, help='입력 이미지의 너비')
+    parser.add_argument('--rgb', action='store_true', help='RGB 입력 사용')
+    parser.add_argument('--character', type=str,default='0123456789abcdefghijklmnopqrstuvwxyz', help='문자 레이블')
+    parser.add_argument('--sensitive', action='store_true', help='민감한 문자 모드')
+    parser.add_argument('--PAD', action='store_true', help='비율 유지 후 이미지 크기 조정을 위한 패딩 여부')
+    parser.add_argument('--data_filtering_off', action='store_true', help='데이터 필터링 끄기 모드')
+    """ 모델 아키텍처 """
+    parser.add_argument('--Transformation', type=str, required=True, help='변환 단계. None|TPS')
+    parser.add_argument('--FeatureExtraction', type=str, required=True, help='특성 추출 단계. VGG|RCNN|ResNet')
+    parser.add_argument('--SequenceModeling', type=str, required=True, help='시퀀스 모델링 단계. None|BiLSTM')
+    parser.add_argument('--Prediction', type=str, required=True, help='예측 단계. CTC|Attn')
+    parser.add_argument('--num_fiducial', type=int, default=20, help='TPS-STN의 fiducial 포인트 수')
+    parser.add_argument('--input_channel', type=int, default=1,help='특성 추출기의 입력 채널 수')
+    parser.add_argument('--output_channel', type=int, default=512,help='특성 추출기의 출력 채널 수')
+    parser.add_argument('--hidden_size', type=int, default=256, help='LSTM의 은닉 상태 크기')
 
     opt = parser.parse_args()
 
@@ -303,7 +298,7 @@ if __name__ == '__main__':
     opt.num_gpu = torch.cuda.device_count()
     # print('device count', opt.num_gpu)
     if opt.num_gpu > 1:
-        print('------ Use multi-GPU setting ------')
+        print('------ 멀티 GPU 사용 ------')
         print('if you stuck too long time with multi-GPU setting, try to set --workers 0')
         # check multi-GPU issue https://github.com/clovaai/deep-text-recognition-benchmark/issues/1
         opt.workers = opt.workers * opt.num_gpu
